366cbfc2ccc25effae838dfb24da167287b708d4
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-05-31 19:34:56 -0300

    add refresh controller

---
+++ app/controllers/refresh_controller.rb
@@ -0,0 +1,20 @@
+class RefreshController < ApplicationController
+    before_action :authorize_refresh_by_access_request!
+
+   def create
+    session = JWTSessions::Sessions.new(
+        payload: claimless_payload,
+        refresh_by_access_allowed: true) 
+
+    tokens = session.refresh_by_access_payload do 
+        raise JWTSessions::Errors::Unauthorized, "Something is not right here!"
+    end
+
+    response.set_cookie(JWTSessions.access_cookie,)
+                        value: tokens[:access],
+                        httponly: true,
+                        secure: Rails.env.production?)
+    
+    render json: { csrf: tokens[:csfr] }
+   end
+end
